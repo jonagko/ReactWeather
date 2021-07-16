9ac5b20fc0517202bc2aa31ecb149472f0db9fc0
jaguirre <jaguirre@vmware.com>
2021-07-16 17:19:02 -0300

    new styles on examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examples locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Montevideo'>Montevideo</Link>
+        </li>
+        <li>
+          <Link to='/?location=California'>California</Link>
+        </li>
+      </ol>
     </div>
   );
 };
